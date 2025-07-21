5326a954a2f45fd73f93a88039b405dd847f6eed
Clinton-Okumu <clintonomondiokumu@gmil.com>
2025-07-21 22:52:43 +0300

    feat(car-assble test): a test on using go types

--- go/cars-assemble/cars_assemble.go
+++ go/cars-assemble/cars_assemble.go
@@ -3,16 +3,21 @@ package cars
 // CalculateWorkingCarsPerHour calculates how many working cars are
 // produced by the assembly line every hour.
 func CalculateWorkingCarsPerHour(productionRate int, successRate float64) float64 {
-	panic("CalculateWorkingCarsPerHour not implemented")
+	successfulCarsMade := float64(productionRate) * float64(successRate/100)
+	return float64(successfulCarsMade)
 }
 
 // CalculateWorkingCarsPerMinute calculates how many working cars are
 // produced by the assembly line every minute.
 func CalculateWorkingCarsPerMinute(productionRate int, successRate float64) int {
-	panic("CalculateWorkingCarsPerMinute not implemented")
+	carsPerMinute := (CalculateWorkingCarsPerHour((productionRate), successRate) / 60)
+	return int(carsPerMinute)
 }
 
 // CalculateCost works out the cost of producing the given number of cars.
 func CalculateCost(carsCount int) uint {
-	panic("CalculateCost not implemented")
+	tens := carsCount / 10
+	ones := carsCount % 10
+	totalCost := (tens * 95000) + (ones * 10000)
+	return uint(totalCost)
 }
